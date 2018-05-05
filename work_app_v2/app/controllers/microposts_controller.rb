c85d6fd2eada4b9bbc92239d2e54fcdae8a8855a
markdeep98 <dmitriy.gladky98@gmail.com>
2018-05-05 07:51:39 -0700

    Almost the end

--- work_app_v2/app/controllers/microposts_controller.rb
+++ work_app_v2/app/controllers/microposts_controller.rb
@@ -20,15 +20,29 @@ class MicropostsController < ApplicationController
 		redirect_to request.referrer || root_url #перенаправить на предыдущую стр. или на главную
 	end
 
+	def edit
+      @micropost = Micropost.find(params[:id])
+    end
+
+	def update
+      @micropost = Micropost.find(params[:id])
+      if @micropost.update_attributes(micropost_params)
+        flash[:success] = "Your question has been updated successfully"
+        redirect_to root_url
+      else
+        render 'edit'
+      end
+    end
+
 	private
 
 	def micropost_params
 		params.require(:micropost).permit(:content)
 	end
 
-	 def correct_user
+	def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
-	
-end
+end	
+
--- work_app_v2/config/routes.rb
+++ work_app_v2/config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts #, only: [:create, :destroy, :update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
